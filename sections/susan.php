31ee22fb50b56532fe6b6897aa6329e5bf87965f
matt jiggins <git@jiggins.ca>
2013-11-14 22:13:06 +0100

    further updates

--- sections/susan.php
+++ sections/susan.php
@@ -6,8 +6,8 @@ $name = "Susan Rock";
 $title = "Operations";
 $phrase = "Canada’s largest provider of CPD Accredited events driving C-IPE";
 $content = "<ul>
-	<li>120 Managed Accredited Events</li>
-	<li>27,025 Total Registrants</li>
+	<li>120 Managed Events</li>
+	<li>27,025+ Total Registrants</li>
 	<li>5 Year CACME Successful Review</li>
 </ul>";
 $contentMobile = "<ul>
